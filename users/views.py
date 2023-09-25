17e57675b39e790ac861653daef74c571064355c
partyTeaTime <6410685215@student.tu.ac.th>
2023-09-25 23:14:36 +0700

    get_error_messages

--- users/views.py
+++ users/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib import messages
 
 # Create your views here.
 def sign_in(request):
     if request.method == 'GET':
         form = LoginForm()
+        for m in messages.get_messages(request):
+            print(m)
         return render(request,'users/login.html', {'form': form})
     
     elif request.method == 'POST':
         form = LoginForm(request.POST)
-        
+        print(bool(messages.get_messages(request)))
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = authenticate(request,username=username,password=password)
             if user is None:
-                form.set_error("Invalid username or password")
-                return render(request,'users/login.html',{'form': form})
+                messages.error(request, "Invalid username or password")
+                for m in messages.get_messages(request):
+                    print(m)
+                return render(request,'users/login.html',{'form': form, 'messages': messages.get_messages(request)})
             
-            form.set_error("")
             if user.is_staff:
                 login(request, user)
                 return redirect('admin:index')
@@ -29,5 +32,7 @@ def sign_in(request):
                 return render(request,'course/course.html')
         
         # form is not valid or user is not authenticated
-        messages = "Invalid username or password"
-        return render(request,'users/login.html',{'form': form, 'messages': messages})
+        messages.error(request, "Invalid username or password")
+        for m in messages.get_messages(request):
+            print(m)
+        return render(request,'users/login.html',{'form': form})
