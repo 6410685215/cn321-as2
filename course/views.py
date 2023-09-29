d63f3d815df094e6f994431025ded25a191b89cd
partyTeaTime <6410685215@student.tu.ac.th>
2023-09-29 22:10:51 +0700

    add admin_page

--- course/views.py
+++ course/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
+from users.models import Student
 
 # Create your views here.
 def index(request):
     return render(request, 'course/course.html')
 
 def page_course(request):
-    return render(request, 'course/page_course.html')
+    user = request.user
+    admin = user.is_staff
+    return render(request, 'course/page_course.html', { 'username': user,
+                                                        'admin': admin,})
 
 def page_user(request):
-    return render(request, 'course/page_user.html')
+    user = request.user
+    admin = user.is_staff
+    if admin:
+        return render(request, 'course/page_user.html', { 'username': user,
+                                                          'admin': admin,})
+    user_s = Student.objects.get(student_id=user)
+    return render(request, 'course/page_user.html', { 'username': user,
+                                                      'admin': admin,
+                                                      'name': user_s.get_full_name(), 
+                                                      'email': user_s.get_email()})
 
 def page_board(request):
-    return render(request, 'course/page_board.html')
+    user = request.user
+    admin = user.is_staff
+    return render(request, 'course/page_board.html', { 'username': user,
+                                                       'admin': admin,})
