0709de5cc124b8256aff63bd2b74b8ab3dbeb530
Thanadon Boontawee <fusethnd@gmail.com>
2023-10-11 00:57:36 +0700

    update users/tests.py

--- users/tests.py
+++ users/tests.py
@@ -25,6 +25,9 @@ class UserTestCase(TestCase):
 
         student1.save()
     
+    def test_models_student_tostr(self):
+        student1 = Student.objects.get(fname="first1")
+        self.assertEqual(str(student1),"user1 | user@mail.com")
 
     def test_annoymous(self):
         c = Client()
@@ -43,44 +46,44 @@ class UserTestCase(TestCase):
         response = c.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
-    def login_get(self):
-        self.client = Client()
-        response = self.client.get("")
+    def test_login_get(self):
+        c = Client()
+        response = c.get("")
         self.assertEqual(response.status_code, 200)
-        # self.assertEqual(, LoginForm())
 
-    def login_as_user(self):
-        self.client = Client()
-        # password = make_password("password1")
+    def test_login_as_user(self):
+        c = Client()
         form = {'username': "user1", 'password' : "password1"}
-        # c.login(username="user1", password="password1")
-        # user = User.objects.get(username="user1")
-        # self.assertEqual(user.is_staff, False)
-        response = self.client.post(reverse('login'), form)
-
+        response = c.post(reverse('login'), form)
         self.assertEqual(response.status_code, 200)
         
     def login_as_staff(self):
         self.client = Client()
-        # password = make_password("password2")
-        form = {'username': "user2", 'password': "password2"}
-        # c.login(username="user2", password="password2")
-        # user = User.objects.get(username="user2")
-        # self.assertEqual(user.is_staff, True)
-        response = self.client.post(reverse('login'), form)
+        # form = {'username': "user2", 'password': "password2"}
+        # user2 = User.objects.get(username='user2')
+        # self.assertEqual(user2.is_staff, True)
+        # login_already = c.login(username="user2", password="password2")
+        # self.assertEqual(login_already, True)
+        # response = c.post(reverse('login'), form)
+        # self.assertEqual(response.status_code, 200)
+        # Attempt login as staff user
+        response = self.client.post(reverse('login'), {'username': 'user2', 'password': 'password2'})
         self.assertEqual(response.status_code, 200)
+        # Check if the staff user is logged in
+        user = User.objects.get(username='user2')
+        self.assertTrue(user.is_authenticated)
+        self.assertTrue(user.is_staff)
+
 
     def test_is_authenticated_wrong_pass(self):
         c = Client()
         form = {'username': "user1", 'password': "wrongpassword"}
-        # c.login(username="user1", password="wrongpassword")
         response = c.post(reverse('login'), form)
         self.assertEqual(response.status_code, 200)
 
     def test_is_authenticated_form_invalid(self):
         c = Client()
         form = {'username': "user1"}
-        # c.login(username="user1", password="wrongpassword")
         response = c.post(reverse('login'), form)
         self.assertEqual(response.status_code, 200)
 
